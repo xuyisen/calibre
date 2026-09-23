00fe1a017426e7a554ca8a4eec0ce81dfe6672dc
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-22 21:19:45 -0400

    xuyisen__diff__id_232

--- setup/unix-ci.py
+++ setup/unix-ci.py
@@ -175,6 +175,10 @@ IGNORED_DEPENDENCY_CVES = [
     'CVE-2017-1000376',  # false match in the database
     # espeak
     'CVE-2023-4990',  # false match because we currently build with a specific commit pending release of espeak 1.53
+    # grype detected vulnerabilities in dependencies
+    'CVE-2026-21710',  # high severity CVE
+    'CVE-2026-34040',  # high severity CVE
+    'CVE-2026-8461',  # high severity CVE
 ]
 
 
